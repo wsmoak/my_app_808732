f5cfb796098d6d8c34588dec83ccbe73a2aa41ee
Wendy Smoak <wsmoak@gmail.com>
2015-08-08 08:49:06 -0400

    Add oauth2 dependency

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule MyApp_808732.Mixfile do
   def application do
     [mod: {MyApp_808732, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :oauth2]]
   end
 
   # Specifies which paths to compile per environment
@@ -34,6 +34,7 @@ defmodule MyApp_808732.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.0"},
      {:phoenix_live_reload, "~> 0.6", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:oauth2, "0.2.0"}]
   end
 end
