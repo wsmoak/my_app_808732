1e8f8b85e75da9304f4663bd45b1a7cc572d009f
Wendy Smoak <wsmoak@gmail.com>
2015-08-08 15:02:45 -0400

    Add user to database

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,6 +1,8 @@
 defmodule MyApp_808732.AuthController do
   use MyApp_808732.Web, :controller
 
+  alias MyApp_808732.User
+
   def index(conn, _params) do
     redirect conn, external: Fitbit.authorize_url!(scope: "settings sleep")
   end
@@ -8,7 +10,17 @@ defmodule MyApp_808732.AuthController do
   def callback(conn, %{"code" => code}) do
     token = Fitbit.get_token!(code: code)
     IO.inspect token
-    redirect conn, to: "/"
+
+    changeset = User.changeset(%User{},
+      %{user_id: token.other_params["user_id"],
+        access_token: token.access_token,
+        refresh_token: token.refresh_token
+      })
+    Repo.insert!(changeset)
+
+    conn
+      |> put_flash(:info, "User created successfully.")
+      |> redirect(to: "/")
   end
 
 end
