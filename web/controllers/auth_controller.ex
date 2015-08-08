8634d15e35b379c2a6b4fe7b6beb31676e96a295
Wendy Smoak <wsmoak@gmail.com>
2015-08-08 14:17:48 -0400

    Add callback function to auth controller

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -4,4 +4,11 @@ defmodule MyApp_808732.AuthController do
   def index(conn, _params) do
     redirect conn, external: Fitbit.authorize_url!(scope: "settings sleep")
   end
+
+  def callback(conn, %{"code" => code}) do
+    token = Fitbit.get_token!(code: code)
+    IO.inspect token
+    redirect conn, to: "/"
+  end
+
 end
